6b09894a93c5ed359120685e3a166fc18a921a6f
Vasilisa Bychik <vbychik@c2r6s4.42prague.com>
2024-05-27 20:05:55 +0200

    ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: vbychik <vbychik@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 21:05:55 by vbychik           #+#    #+#             */
-/*   Updated: 2024/05/24 21:09:55 by vbychik          ###   ########.fr       */
+/*   Updated: 2024/05/27 19:59:20 by vbychik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 char	strlcpy(char *dest, const char *scr, size_t n)
 {
-	//...
+	size_t	i;
+
+	i = ft_strlen(src);
+	if (i +1 < n)
+		ft_memcpy (dst, src, i +1);
+	else if (n != 0)
+	{
+		ft_memcpy (dst, src, n - 1);
+		dst[n - 1] = 0;
+	}
+	return (i);
 }
