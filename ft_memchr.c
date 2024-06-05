df4af9de86d46bf785e6deb3c5406b393d15c649
Basilius23 <166595386+Basilius23@users.noreply.github.com>
2024-06-05 15:54:09 +0200

    Update ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* *********************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbychik <vbychik@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/01 20:21:22 by vbychik           #+#    #+#             */
+/*   Updated: 2024/06/01 20:47:10 by vbychik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void    *ft_memchr(const void *s, int c, size_t n)
+{
+    size_t    i;
+    unsigned char    str;
+    unsigned char    uc;
+    
+    str = (unsigned char *) s;
+    uc (unsigned char) c;
+    i = 0;
+    
+    while (i < n)
+    {
+        if (str[i] == uc)
+            return ((void *) str[i]);
+        i++;
+    }
+    return (NULL);
+}
