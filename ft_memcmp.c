8301d6a1eb189e1f0d5ce935c48a18e31762ee31
Basilius23 <166595386+Basilius23@users.noreply.github.com>
2024-06-06 12:16:15 +0200

    Update ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbychik <vbychik@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 19:35:03 by vbychik           #+#    #+#             */
+/*   Updated: 2024/05/24 21:05:29 by vbychik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int    ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    unsigned char    *str1;
+    unsigned char    *str2;
+    size_t    i;
+    
+    str1 = (unsigned char) *s1;
+    str2 = (unsigned char) *s2;
+    i = 0;
+    
+    while (i < n)
+    {
+        if ((unsigned char) str1[i] != (unsigned char) str2[i])
+            return ((unsigned char) str1[i] - (unsigned char) str2[i]);
+    }
+    return (0);
+}
